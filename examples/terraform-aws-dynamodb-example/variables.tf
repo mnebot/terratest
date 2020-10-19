1173cfca3383908c2878ad3830cc8aa86cbb9c47
Marçal Nebot <mnebot@gmail.com>
2020-10-19 13:26:05 +0200

    Update variables.tf

--- examples/terraform-aws-dynamodb-example/variables.tf
+++ examples/terraform-aws-dynamodb-example/variables.tf
@@ -6,6 +6,6 @@
 variable "table_name" {
   description = "The name to set for the dynamoDB table."
   type        = string
-  default     = "terratest-example"
+  default     = "terratest-example-error" # replace with terratest-example to make it pass the unit tests
 }
 
